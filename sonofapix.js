b178701ad1ffbb1fe4e48787534a9d3f9faf52e6
Franck Lagora <lagora.franck@gmail.com>
2015-05-27 23:34:02 +0200

    Update sonofapix.js

--- sonofapix.js
+++ sonofapix.js
@@ -1,23 +1,18 @@
 var sonofapix = {
-  // "head": {
-  //   "link": [
-  //     {"type": "text/css","rel": "stylesheet","href": "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"},
-  //     {"type": "text/css","rel": "stylesheet","href": "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css"}
-  //   ],
-  //   "script": [
-  //     {"type": "text/javascript", "src": "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"},
-  //     {"type": "text/javascript", "src": "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"}
-  //   ]
-  // },
   "links": {
     "mail": {"icon": "fa fa-envelope", "href": "mailto:lagora@sonofapix.com"},
     "twitter": {"icon": "fa fa-twitter", "href": "http://www.twitter.com/sonofapix"},
-    "tumblr": {"icon": "fa fa-tumblr", "href": "http://sonofapix.tumblr.com/sonofapix"}
+    "tumblr": {"icon": "fa fa-tumblr", "href": "http://sonofapix.tumblr.com/sonofapix"},
+    "instagram": {"icon": "fa fa-instagram", "href": "https://instagram.com/sonofapix"},
   },
   "projects": {},
   "init": function () {
     console.log('init');
-    Object.keys(sonofapix.links).map(function (each_link) {
+    Object.keys(sonofapix.links)
+    .filter(function (each_link) {
+      return 'instagram' !== each_link;
+    })
+    .map(function (each_link) {
       var link_icon = document.createElement('i');
       link_icon.classList = sonofapix.links[each_link].icon;
       
@@ -36,13 +31,3 @@ var sonofapix = {
     });
   }
 };
-
-// Object.keys(sonofapix.head).map(function (head_element) {
-//   sonofapix.head[head_element].map(function (each_element) {
-//     var element = document.createElement(head_element);
-//     Object.keys(each_element).map(function (element_attribute) {
-//       element[element_attribute] = each_element[element_attribute];
-//     });
-//     document.head.appendChild(element);
-//   });
-// });

0a3f464f2b329348df775372a800fa90cec6ffb9
lagora <lagora.franck@gmail.com>
2015-05-30 18:12:08 +0200

    wip

--- blog.js
+++ blog.js
@@ -1,3 +1,35 @@
-var posts = [];
+var blogPosts = new List('blog', {
+    valueNames: ['blog-post'],
+    page: 1,
+    i: posts.length-1,
+    plugins: [ ListPagination({})],
+    item: '<li><p><span class="datetime"></span><br><small class="tags hidden"></small></p><p><small class="content"></small></p></li>'
+});
 
-posts.map(function () {});
+console.log(window.posts);
+
+window.posts.map(function (post) {
+    $.ajax({
+        url: 'blog/' + post.file
+    })
+    .done(function (content) {
+        if (!isNaN(post.datetime)) {
+            post.datetime =  moment.unix(post.datetime).format('YYYY-MM-DD HH:mm');
+        }
+
+        content = content.split('.').map(function (a) {return a.trim().substring(0,1).toUpperCase()+a.substring(1);}).join('. ');
+
+        post.content = marked(content);
+        var opts = {
+            datetime: post.datetime,
+            content: post.content
+        };
+
+        if (post.tags) {
+            opts.tags = post.tags.split(',').map(function(a){return '<a>'+a.trim()+'</a>'});
+        }
+        console.log(opts.tags);
+
+        blogPosts.add(opts);
+    });
+});
---
+++ blog/hello_world.md
@@ -0,0 +1,1 @@
+the website is now better and...that's all. it's very basic, most of the stuff is done in your browser (eh, why not ?).

08a19af5e5c835f1d616b6bc987ded148f389b5b
lagora <lagora.franck@gmail.com>
2015-05-31 19:19:12 +0200

    .

--- dev.js
+++ dev.js
@@ -1,10 +1,9 @@
 var static = require('node-static');
-
 var file = new static.Server('./');
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
-        console.log('restarted');
+        console.log('restarted @', (new Date()).toString());
         file.serve(request, response);
     }).resume();
 }).listen(80);
--- js/sonofapix.js
+++ js/sonofapix.js
@@ -13,20 +13,21 @@ var sonofapix = {
       return 'instagram' !== each_link;
     })
     .map(function (each_link) {
+
       var link_icon = document.createElement('i');
       link_icon.classList = 'fa ' + sonofapix.links[each_link].icon;
-      
+
       var link_a = document.createElement('a');
       link_a.href = sonofapix.links[each_link].href;
       link_a.target = "_blank";
-      
+
       var link_div = document.createElement('div');
       link_div.classList = "col-xs-1";
       console.log(link_icon, link_a, link_div);
-      
+
       link_a.appendChild(link_icon);
       link_div.appendChild(link_a);
-      
+
       document.getElementById('links').appendChild(link_div);
     });
   }

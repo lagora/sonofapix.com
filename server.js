1752dffc324bc7291c820476bf24e23ec8e0ce9f
Franck Lagora <lagora@users.noreply.github.com>
2016-02-26 23:31:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-  //nodejs.org/api for API docs
-  //Node.js web server                         
-  var http = require("http"),                           //Import Node.js modules
-      url = require("url"),                             
-      path = require("path"),
-      fs = require("fs");
+var fs = require('fs');
+var index = fs.readFileSync('index.html', 'utf8');
+//Lets define a port we want to listen to
+const PORT=80; 
 
-  http.createServer(function(request, response) {       //Create server 
-  var name = url.parse(request.url).pathname;           //Parse URL
-  var filename = path.join(process.cwd(), name);        //Create filename
-  fs.readFile(filename, "binary", function(err, file) { //Read file
-      if(err) {                                         //Tracking Errors
-          response.writeHead(500, {"Content-Type": "text/plain"});
-          response.write(err + "\n");
-          response.end();
-          return;
-      }
-      response.writeHead(200);                          //Header request response 
-      response.write(file, "binary");                   //Sends body response
-      response.end();                                   //Signals to server that 
-   });                                                  //header and body sent
-  }).listen(3000);                                      //Listening port 
-  console.log("Server is listening on port 3000.")      //Terminal output
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+  response.end(index);
+}
+
+//Lets start our server
+require('http').createServer(handleRequest).listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
